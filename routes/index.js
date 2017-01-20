4a254207c452d932fd151670b5ded118094fd011
Raviteja <raviteja.ravi143@gmail.com>
2017-01-20 23:27:42 +0000

    add retypepassword

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,10 @@ router.get("/register", function(req, res) {
     res.render("register");
 });
 router.post("/register", function(req, res) {
+    if (req.body.password !== req.body.password2) {
+        req.flash("error", "Password's do not match");
+        res.redirect('/register');
+    }else {
     var newUser = new User({username: req.body.username})
     User.register(newUser, req.body.password, function(err, user){
         if(err){
@@ -21,11 +25,13 @@ router.post("/register", function(req, res) {
             req.flash("error", err.message);
             return res.render("register")
         }
-        passport.authenticate("local")(req,res,function(){
+       
+            passport.authenticate("local")(req,res,function(){
             req.flash("success", "welcome to YelpCamp"  + user.username);
             res.redirect("/campgrounds");
-        })
+        }) 
     })
+    }
 })
 //=======login routes=======
 router.get("/login",function(req, res) {
